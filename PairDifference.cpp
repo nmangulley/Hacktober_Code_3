74d5b15f3cd10dcb81e10e46d3dce2dcec33fba5
Niketan0109 <56088377+Niketan0109@users.noreply.github.com>
2022-10-01 12:51:15 +0530

    Update PairDifference.cpp

--- PairDifference.cpp
+++ PairDifference.cpp
@@ -17,6 +17,24 @@ int binarySearch(int arr[], int low, int high, int val)
     return -1;
 }
 
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+       int l=0;
+       int r = arr.size()-1;
+       while(r-l>=k)
+       {
+          if(x-arr[l]<=arr[r]-x)
+          {
+              r--;
+          }
+           else{
+               l++;
+           }
+       }
+        return vector<int>(begin(arr)+l,begin(arr)+r+1);
+    }
+};
 
 
 int main()
